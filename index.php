7333e83db557639c49b7b06c793eef605db98c96
Hannah <hannahrspringer@gmail.com>
2016-09-07 18:27:03 -0500

    Moved position of comment

--- index.php
+++ index.php
@@ -35,10 +35,11 @@
   ?>
 </p>
 
-//Create a class called Cat. Add two public properties to this class: $isAlive ought to store the value true and $numLegsshould contain the value 4. Add a public $name property, which gets its value via the __construct()or. Add a public method called meow(), which returns "Meow meow". Create an instance of the Cat class, which has the $name "CodeCat". Call the meow() method on this Cat and echo the result.
-
 <p>
         <?php
+
+        //Create a class called Cat. Add two public properties to this class: $isAlive ought to store the value true and $numLegsshould contain the value 4. Add a public $name property, which gets its value via the __construct()or. Add a public method called meow(), which returns "Meow meow". Create an instance of the Cat class, which has the $name "CodeCat". Call the meow() method on this Cat and echo the result.
+
         class Cat {
             public $isAlive = true;
             public $numLegs = 4;
